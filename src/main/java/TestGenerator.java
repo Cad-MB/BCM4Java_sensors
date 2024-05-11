3fac5d24d4aa9d3a55d93cdcfaf27286fe0b96cc
Abdelkader Boumessaoud <abdelkader.boumessaoud.pro@gmail.com>
2024-05-11 12:11:25 +0200

    TestGenerator : 5 clients, 50 nœuds -> a corriger

--- src/main/java/TestGenerator.java
+++ src/main/java/TestGenerator.java
@@ -14,6 +14,7 @@ public class TestGenerator {
 
     public static void main(String[] args) throws JAXBException {
         stressTest1();
+        stressTest2();
     }
 
     private static void stressTest1() throws JAXBException {
@@ -25,7 +26,7 @@ public class TestGenerator {
         for (int i = 0; i < 5; i++) {
             String nodeId = "node-" + i;
             forest.nodes.add(
-                new NodeParser.Node(nodeId, "plugin-" + nodeId, new NodeParser.Threads(2, 2), 100, new NodeParser.Position(i, i), (long) (60 + (i * 10)),
+                new NodeParser.Node(nodeId, "plugin-" + nodeId, new NodeParser.Threads(2, 2), 100, new NodeParser.Position(i*100, i*100), (long) (60 + (i * 10)),
                                     (long) (4000 + (60 + (i * 10)) * 2), 100L, new ArrayList<NodeParser.Sensor>() {{
                     add(new NodeParser.Sensor("temp", 10f, 2f));
                     add(new NodeParser.Sensor("humidity", 10f, 2f));
@@ -75,10 +76,10 @@ public class TestGenerator {
                                                     clientId + ":" + node2,
                                                     true,
                                                     "bool @temp > 10 dir ne 2",
-                                                    100));
+                                                    400));
                     }},
                     100,
-                    100,
+                    1000,
                     new ArrayList<ClientParser.Port>() {{
                         PortName reqResult = PortName.REQUEST_RESULT;
                         add(new ClientParser.Port(reqResult, clientId + ":inbound:" + reqResult.xmlName()));
@@ -134,5 +135,132 @@ public class TestGenerator {
 
         testMarshaller.marshal(tests, new File(baseDir+"tests.xml"));
 
+    }
+    private static void stressTest2() throws JAXBException {
+        String baseDir = "src/main/resources/configs/stress_test_2/";
+        new File(baseDir).mkdirs();
+
+        int gridWidth = 10;
+
+        // Nodes
+        NodeParser.Forest forest = new NodeParser.Forest();
+        forest.nodes = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+
+            int x;
+            int y = i / 10;
+            if (y % 2 == 0)
+            {x = ((i % 10)*2)+1;}
+            else {x = (i % 10)*2;}
+
+            String nodeId = "node-" + i;
+            forest.nodes.add(new NodeParser.Node(
+                nodeId, "plugin-" + nodeId,
+                new NodeParser.Threads(2, 2), 200,
+                new NodeParser.Position(x*100, y*100), (long) (400 + (i * 10)),
+                (long) (4000 + (60 + (i * 10)) * 2), 100L,
+                new ArrayList<NodeParser.Sensor>() {{
+                    add(new NodeParser.Sensor("temp", 10f, 2f));
+                    add(new NodeParser.Sensor("humidity", 10f, 2f));
+                }},
+                new ArrayList<NodeParser.Port>() {{
+                    PortName requesting = PortName.REQUESTING;
+                    PortName p2p = PortName.P2P;
+                    add(new NodeParser.Port(requesting, nodeId + ":inbound:" + requesting.xmlName()));
+                    add(new NodeParser.Port(p2p, nodeId + ":inbound:" + p2p.xmlName()));
+                }},
+                new ArrayList<NodeParser.Port>() {{
+                    PortName result = PortName.REQUEST_RESULT;
+                    PortName p2p = PortName.P2P;
+                    PortName registration = PortName.REGISTRATION;
+                    PortName clock = PortName.CLOCK;
+                    add(new NodeParser.Port(result, nodeId + ":outbound:" + result.xmlName()));
+                    add(new NodeParser.Port(p2p, nodeId + ":outbound:" + p2p.xmlName()));
+                    add(new NodeParser.Port(registration, nodeId + ":outbound:" + registration.xmlName()));
+                    add(new NodeParser.Port(clock, nodeId + ":outbound:" + clock.xmlName()));
+                }}
+            ));
+        }
+
+        JAXBContext forestCtx = JAXBContext.newInstance(NodeParser.Forest.class);
+        Marshaller forestCtxMarshaller = forestCtx.createMarshaller();
+        forestCtxMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        forestCtxMarshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+        forestCtxMarshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+        forestCtxMarshaller.marshal(forest, new File(baseDir + "forest.xml"));
+
+        // Clients
+        ClientParser.Clients clients = new ClientParser.Clients();
+        clients.clients = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            String clientId = "client-" + i;
+            int finalI = i;
+            clients.clients.add(new ClientParser.Client(
+                clientId, "plugin-" + clientId,
+                new NodeParser.Threads(2, 2),
+                new ArrayList<ClientParser.Target>() {{
+                    String node1 = "node-" + (finalI * 10);
+                    String node2 = "node-" + (finalI * 10 + 1);
+                    add(new ClientParser.Target(node1,
+                                                clientId + ":" + node1,
+                                                true,
+                                                "bool @temp > 10 dir ne 2",
+                                                100));
+                    add(new ClientParser.Target(node2,
+                                                clientId + ":" + node2,
+                                                true,
+                                                "bool @temp > 10 dir ne 2",
+                                                100));
+                }},
+                100,
+                100,
+                new ArrayList<ClientParser.Port>() {{
+                    PortName reqResult = PortName.REQUEST_RESULT;
+                    add(new ClientParser.Port(reqResult, clientId + ":inbound:" + reqResult.xmlName()));
+                }},
+                new ArrayList<ClientParser.Port>() {{
+                    PortName lookup = PortName.LOOKUP;
+                    PortName clock = PortName.CLOCK;
+                    add(new ClientParser.Port(lookup, clientId + ":outbound:" + lookup.xmlName()));
+                    add(new ClientParser.Port(clock, clientId + ":outbound:" + clock.xmlName()));
+                }}
+            ));
+        }
+
+        JAXBContext clientCtx = JAXBContext.newInstance(ClientParser.Clients.class);
+        Marshaller clientCtxMarshaller = clientCtx.createMarshaller();
+        clientCtxMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        clientCtxMarshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+        clientCtxMarshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+        clientCtxMarshaller.marshal(clients, new File(baseDir + "client.xml"));
+
+        // Tests
+        TestParser.Tests tests = new TestParser.Tests();
+        tests.testList = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            String clientId = "client-" + i;
+            String node1 = "node-" + (i * 10);
+            String node2 = "node-" + (i * 10 + 1);
+            tests.testList.add(new TestParser.Test(
+                clientId + ":test" + i,
+                clientId,
+                clientId + "-0",
+                550 + (i * 100),
+                true,
+                Arrays.asList(node1, node2),
+                null
+            ));
+        }
+
+        tests.executionDuration = 50000L;
+
+        JAXBContext testCtx = JAXBContext.newInstance(TestParser.Tests.class);
+        Marshaller testMarshaller = testCtx.createMarshaller();
+        testMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        testMarshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+        testMarshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+
+        testMarshaller.marshal(tests, new File(baseDir + "tests.xml"));
     }
 }
